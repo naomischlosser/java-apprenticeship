3f60be5666aa01411ca3b1ba5a674cfdcde1ab8d
naomischlosser <naomi_schlosser@hotmail.com>
2022-11-01 17:34:11 +0100

    change Calculator file names to be unique

--- week1/step1/Calculator1.java
+++ week1/step1/Calculator1.java
@@ -1,4 +1,4 @@
-class Calculator {
+class Calculator1 {
   public static void main(String[] args) {
     Integer result = 2 + 2;
     System.out.printf("The result is %d \n", result);
--- week1/step2/Calculator2.java
+++ week1/step2/Calculator2.java
@@ -1,4 +1,4 @@
-class Calculator {
+class Calculator2 {
   public static void main(String[] args) {
     Double result = 2.5 + 2.5;
     System.out.printf("The result is %f \n", result);

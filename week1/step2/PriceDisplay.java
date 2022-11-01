f283c4e9d7bbd1a629503d98fbe782b721e958a8
naomischlosser <naomi_schlosser@hotmail.com>
2022-11-01 16:51:46 +0100

    add PriceDisplay class

---
+++ week1/step2/PriceDisplay.java
@@ -0,0 +1,8 @@
+class PriceDisplay {
+  public static void main(String[] args) {
+    String item = "Coffee Machine";
+    Float price = 15.99f;
+
+    System.out.printf("The price for the item %s is %.2f\n", item, price);
+  }
+}

e1458c3cc766620e3d05fbfb70a802702708dcc3
naomischlosser <naomi_schlosser@hotmail.com>
2022-11-01 17:08:48 +0100

    fix type error

--- week1/step2/LongestName.java
+++ week1/step2/LongestName.java
@@ -2,7 +2,7 @@ class LongestName {
   public String longestName(String[] names) {
     String longest = "";
     for (String name : names) {
-      if (longest.length() < name) {
+      if (longest.length() < name.length()) {
         longest = name;
       }
     }

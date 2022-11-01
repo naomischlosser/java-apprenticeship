2cb589a5fa1b97f56664c720000e01dc5ea7e338
naomischlosser <naomi_schlosser@hotmail.com>
2022-11-01 16:19:55 +0100

    add colour to time in string

--- week1/step1/Clock.java
+++ week1/step1/Clock.java
@@ -2,11 +2,13 @@ import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
 class Clock {
+  public static final String ANSI_RED = "\u001B[31m";
+  public static final String ANSI_RESET = "\u001B[0m";
   public static void main(String[] args) {
     LocalTime myDateObj = LocalTime.now();
     DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("HH:mm");
     
     String formattedTime = myDateObj.format(myFormatObj);
-    System.out.printf("The current time is %s \n", formattedTime);
+    System.out.printf("The current " + ANSI_RED + "time " + ANSI_RESET + "is %s \n", formattedTime);
   }
 }

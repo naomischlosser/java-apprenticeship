9d5fac367e8b786e0073f81581f4bb1cc39a3798
naomischlosser <naomi_schlosser@hotmail.com>
2022-11-02 08:17:18 +0000

    test passing

--- week1/step4/store/app/src/main/java/store/PriceDisplay.java
+++ week1/step4/store/app/src/main/java/store/PriceDisplay.java
@@ -1,5 +1,7 @@
 package store;
 
 public class PriceDisplay {
-
+  public String getPriceLabel(String item, int price) {
+    return String.format("The price for item: %s is %d", item, price);
+  }
 }

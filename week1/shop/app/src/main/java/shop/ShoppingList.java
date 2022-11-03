b203bafa1a656dfae84837d239c4e096bf596538
naomischlosser <naomi_schlosser@hotmail.com>
2022-11-03 16:05:06 +0000

    test initial empty shopping list passes

--- week1/shop/app/src/main/java/shop/ShoppingList.java
+++ week1/shop/app/src/main/java/shop/ShoppingList.java
@@ -1,5 +1,11 @@
 package shop;
 
+import java.util.ArrayList;
+
 public class ShoppingList {
+  private ArrayList<String> items = new ArrayList<String>();
   
+  public ArrayList<String> getItems() {
+    return items;
+  }
 }
--- week1/shop/app/src/test/java/shop/ShoppingListTest.java
+++ week1/shop/app/src/test/java/shop/ShoppingListTest.java
@@ -1,5 +1,11 @@
 package shop;
 
+import static org.junit.Assert.*;
+import org.junit.*;
+
 public class ShoppingListTest {
-  
+  @Test public void testGetsInitialEmptyShoppingList() {
+    ShoppingList shoppingList = new ShoppingList();
+    assertTrue(shoppingList.getItems().isEmpty());
+  }
 }

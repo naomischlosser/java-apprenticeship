c725e557d77332d446f773fc49af25699e1f678b
naomischlosser <naomi_schlosser@hotmail.com>
2022-11-17 11:21:39 +0000

    start of 2 player game - create player class

---
+++ week1/game/app/src/main/java/game/Player.java
@@ -0,0 +1,7 @@
+package game;
+
+public class Player {
+    public int randomisePlayer() {
+        return (Math.random() <= 0.5) ? 0 : 1;
+    }
+}
---
+++ week1/game/app/src/test/java/game/PlayerTest.java
@@ -0,0 +1,13 @@
+package game;
+
+import static org.junit.Assert.*;
+import java.util.Arrays;
+import org.junit.Test;
+
+public class PlayerTest {
+    @Test public void testRandomPlayer() {
+        Player player = new Player();
+        Integer[] MOCK_PLAYERS = {0, 1};
+        assertTrue(Arrays.asList(MOCK_PLAYERS).contains(player.randomisePlayer()));
+    }
+}
